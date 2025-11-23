2abcd3856b577c5a243f1f13dfc02ec46818e2de
bruderj15 <julian.bruder@outlook.com>
2025-11-23 18:20:20 +0100

    main: proper state-typing for node-funs

--- src/app.ts
+++ src/app.ts
@@ -6,27 +6,34 @@ const rl = readline.createInterface({ input, output });
 
 const AgentState = Annotation.Root({
   user_name: Annotation<string | undefined>(),
-  pizza_name: Annotation<string | undefined>(),
+  current_pizza_name: Annotation<string | undefined>(),
+  pizzas: Annotation<PizzaData[] | undefined>(),
   output: Annotation<string[]>(),
 });
 
 const ASK_USER_NAME_NODE = "ask_user_name";
-async function askUserName(state: any) {
+async function askUserName(state: typeof AgentState.State) {
   const name = await rl.question("ChatBot: What's your name?\n");
   console.info("Received:", name);
   state.user_name = name;
   return state;
 }
 
-function shouldAskUserName(state: any) {
+function shouldAskUserName(state: typeof AgentState.State) {
   return state.user_name ? GREETING_NODE : ASK_USER_NAME_NODE;
+}
+
+const GREETING_NODE = "greeting";
+async function greetingNode(state: typeof AgentState.State) {
+  state.output.push(`Hello, ${state.user_name}!`);
+  return state;
 }
 
 interface PizzaData {
   id: number;
   name: String;
 }
-async function shouldAskPizzaName(state: any) {
+async function shouldAskPizzaName(state: typeof AgentState.State) {
   const getPizzasRes = await fetch(
     "https://demos.swe.htwk-leipzig.de/pizza-api/pizza",
   );
@@ -35,42 +42,36 @@ async function shouldAskPizzaName(state: any) {
   }
 
   const valid_pizzas: PizzaData[] = (await getPizzasRes.json()) as PizzaData[];
-  if (!state.pizza_name) {
+  if (!state.current_pizza_name) {
     return ASK_PIZZA_NAME_NODE;
   } else {
     let valid_pizza_names = valid_pizzas.map((pizza) => pizza.name);
     let is_valid = valid_pizza_names.some(
-      (pizza_name) => pizza_name == state.pizza_name,
+      (pizza_name) => pizza_name == state.current_pizza_name,
     );
     if (is_valid) {
       return END;
     } else {
       console.info(
-        `Pizza '${state.pizza_name}' is invalid. Try any of: '${valid_pizza_names}'`,
+        `Pizza '${state.current_pizza_name}' is invalid. Try any of: '${valid_pizza_names}'`,
       );
       return ASK_PIZZA_NAME_NODE;
     }
   }
 }
 
-const GREETING_NODE = "greeting";
-async function greetingNode(state: any) {
-  state.output.push(`Hello, ${state.user_name}!`);
-  return state;
-}
-
 const ASK_PIZZA_NAME_NODE = "ask_pizza_name";
-async function askPizzaName(state: any) {
+async function askPizzaName(state: typeof AgentState.State) {
   const pizza = await rl.question(
     "ChatBot: What pizza do you want to order?\n",
   );
   console.info("Received:", pizza);
-  state.pizza_name = pizza;
+  state.current_pizza_name = pizza;
   return state;
 }
 
 const VALIDATE_PIZZA_NAME_NODE = "validate_pizza_name";
-async function validatePizzaName(state: any) {
+async function validatePizzaName(state: typeof AgentState.State) {
   return state;
 }
 
